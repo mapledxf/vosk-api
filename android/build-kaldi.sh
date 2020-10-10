53ba2ef7dd3f9a2e7369ba7931bbf9d5be2e83d6
xfding <xfding@vw-mobvoi.com>
2020-10-10 11:16:40 +0800

    new version

--- android/build-kaldi.sh
+++ android/build-kaldi.sh
@@ -15,132 +15,162 @@
 # limitations under the License.
 
 if [ "x$ANDROID_SDK_HOME" == "x" ]; then
-    echo "ANDROID_SDK_HOME environment variable is undefined, define it with local.properties or with export"
-    exit 1
+    	echo "ANDROID_SDK_HOME environment variable is undefined, define it with local.properties or with export"
+    	exit 1
 fi
 
 if [ ! -d "$ANDROID_SDK_HOME" ]; then
-    echo "ANDROID_SDK_HOME ($ANDROID_SDK_HOME) is missing. Make sure you have sdk installed"
-    exit 1
+	echo "ANDROID_SDK_HOME ($ANDROID_SDK_HOME) is missing. Make sure you have sdk installed"
+    	exit 1
 fi
 
-if [ ! -d "$ANDROID_SDK_HOME/ndk-bundle" ]; then
-    echo "$ANDROID_SDK_HOME/ndk-bundle is missing. Make sure you have ndk installed within sdk"
-    exit 1
+if [ "x$ANDROID_NDK_HOME" == "x" ]; then
+    	echo "ANDROID_NDK_HOME environment variable is undefined, define it with local.properties or with export"
+    	exit 1
+fi
+
+if [ ! -f "$ANDROID_NDK_HOME/ndk-build" ]; then
+    	echo "$ANDROID_NDK_HOME/ndk-build is missing. Make sure you have ndk installed within sdk"
+    	exit 1
 fi
 
 set -x
 
-ANDROID_NDK_HOME=$ANDROID_SDK_HOME/ndk-bundle
-ANDROID_TOOLCHAIN_PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
+OS_NAME=`echo $(uname -s) | tr '[:upper:]' '[:lower:]'`
+ANDROID_TOOLCHAIN_PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/${OS_NAME}-x86_64
+WORKDIR_X86=`pwd`/build/kaldi_x86
 WORKDIR_X86_64=`pwd`/build/kaldi_x86_64
 WORKDIR_ARM32=`pwd`/build/kaldi_arm_32
 WORKDIR_ARM64=`pwd`/build/kaldi_arm_64
-PATH=$PATH:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
+PATH=$PATH:$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/${OS_NAME}-x86_64/bin
 OPENFST_VERSION=1.6.7
+
+ANDROID_LIBS_PATH=`pwd`/build/android_libs
 OPEN_BLAS_PATH=`pwd`/build/OpenBLAS
-OPEN_FST_PATH=`pwd`/build/openfst-${OPENFST_VERSION}
+OPEN_FST_PATH=`pwd`/build/openfst
 KALDI_PATH=`pwd`/build/kaldi
 
-mkdir -p $WORKDIR_ARM64/local/lib $WORKDIR_ARM32/local/lib $WORKDIR_X86_64/local/lib
-
-# Build standalone CLAPACK since gfortran is missing
+mkdir build
 cd build
-if [ ! -d "android_libs" ]; then
-    git clone https://github.com/simonlynen/android_libs
-fi
-cd android_libs/lapack
-sed -i 's/APP_STL := gnustl_static/APP_STL := c++_static/g' jni/Application.mk && \
-sed -i 's/android-10/android-21/g' project.properties && \
-sed -i 's/APP_ABI := armeabi armeabi-v7a/APP_ABI := armeabi-v7a arm64-v8a x86_64/g' jni/Application.mk && \
-sed -i 's/LOCAL_MODULE:= testlapack/#LOCAL_MODULE:= testlapack/g' jni/Android.mk && \
-sed -i 's/LOCAL_SRC_FILES:= testclapack.cpp/#LOCAL_SRC_FILES:= testclapack.cpp/g' jni/Android.mk && \
-sed -i 's/LOCAL_STATIC_LIBRARIES := lapack/#LOCAL_STATIC_LIBRARIES := lapack/g' jni/Android.mk && \
-sed -i 's/include $(BUILD_SHARED_LIBRARY)/#include $(BUILD_SHARED_LIBRARY)/g' jni/Android.mk && \
-${ANDROID_NDK_HOME}/ndk-build && \
-cp obj/local/armeabi-v7a/*.a ${WORKDIR_ARM32}/local/lib && \
-cp obj/local/arm64-v8a/*.a ${WORKDIR_ARM64}/local/lib
-cp obj/local/x86_64/*.a ${WORKDIR_X86_64}/local/lib
-
-cd ../..
-if [ ! -d 'OpenBLAS' ]; then
-    git clone -b v0.3.7 --single-branch https://github.com/xianyi/OpenBLAS
+# Build standalone CLAPACK since gfortran is missing
+if [ ! -d $ANDROID_LIBS_PATH ]; then
+    	git clone https://github.com/mapledxf/android_libs.git
 fi
 
-if [ ! -d 'openfst-${OPENFST_VERSION}' ]; then
-    wget -c -T 10 -t 3 http://www.openslr.org/resources/2/openfst-${OPENFST_VERSION}.tar.gz || \
-    wget -c -T 10 -t 1 http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-${OPENFST_VERSION}.tar.gz
-    tar -zxvf openfst-${OPENFST_VERSION}.tar.gz
+if [ ! -d $OPEN_BLAS_PATH ]; then
+    	git clone -b v0.3.7 --single-branch https://github.com/mapledxf/OpenBLAS.git 
+fi
+
+if [ ! -d $OPEN_FST_PATH ]; then
+	git clone https://github.com/mapledxf/openfst.git
 fi
 
 if [ ! -d ${KALDI_PATH} ]; then
-    git clone -b android-mix --single-branch https://github.com/alphacep/kaldi
+    	git clone -b android-mix --single-branch https://github.com/alphacep/kaldi
 fi
 
 # Architecture-specific part
-for arch in arm32 arm64 x86_64; do
-    #for arch in x86_64; do
-    case $arch in
-        arm32)
-            BLAS_ARCH=ARMV7
-            WORKDIR=$WORKDIR_ARM32
-            HOST=arm-linux-androideabi
-            AR=arm-linux-androideabi-ar
-            CC=armv7a-linux-androideabi21-clang
-            CXX=armv7a-linux-androideabi21-clang++
-            ARCHFLAGS="-mfloat-abi=softfp -mfpu=neon"
-            ;;
-        arm64)
-            BLAS_ARCH=ARMV8
-            WORKDIR=$WORKDIR_ARM64
-            HOST=aarch64-linux-android
-            AR=aarch64-linux-android-ar
-            CC=aarch64-linux-android21-clang
-            CXX=aarch64-linux-android21-clang++
-            ARCHFLAGS=""
-            ;;
-        x86_64)
-            BLAS_ARCH=ATOM
-            WORKDIR=$WORKDIR_X86_64
-            HOST=x86_64-linux-android
-            AR=x86_64-linux-android-ar
-            CC=x86_64-linux-android21-clang
-            CXX=x86_64-linux-android21-clang++
-            ARCHFLAGS=""
-            ;;
-    esac
+#for arch in arm32 arm64 x86_64 x86; do
+for arch in x86_64; do
+	case $arch in
+		arm32)
+			BLAS_ARCH=ARMV7
+			WORKDIR=$WORKDIR_ARM32
+			HOST=arm-linux-androideabi
+			AR=arm-linux-androideabi-ar
+			CC=armv7a-linux-androideabi21-clang
+			CXX=armv7a-linux-androideabi21-clang++
+			ARCHFLAGS="-mfloat-abi=softfp -mfpu=neon"
+			COMPILE_ABI=armeabi-v7a
+			;;
+		arm64)
+	    		BLAS_ARCH=ARMV8
+			WORKDIR=$WORKDIR_ARM64
+	    		HOST=aarch64-linux-android
+	    		AR=aarch64-linux-android-ar
+	    		CC=aarch64-linux-android21-clang
+	    		CXX=aarch64-linux-android21-clang++
+	    		ARCHFLAGS=""
+			COMPILE_ABI=arm64-v8a
+	    		;;
+		x86_64)
+	    		BLAS_ARCH=ATOM
+	    		WORKDIR=$WORKDIR_X86_64
+	    		HOST=x86_64-linux-android
+	    		AR=x86_64-linux-android-ar
+	    		CC=x86_64-linux-android21-clang
+	    		CXX=x86_64-linux-android21-clang++
+	    		ARCHFLAGS=""
+			COMPILE_ABI=x86_64
+	    		;;
+	    	x86)
+	      		BLAS_ARCH=ATOM
+	      		WORKDIR=$WORKDIR_X86
+	      		HOST=i686-linux-android
+	      		AR=i686-linux-android-ar
+	      		CC=i686-linux-android21-clang
+	      		CXX=i686-linux-android21-clang++
+	      		ARCHFLAGS=""
+			COMPILE_ABI=x86
+	      		;;
+    	esac
 
-    # openblas first
-    cd $WORKDIR
-    cp -r ${OPEN_BLAS_PATH} .
-    make -C OpenBLAS TARGET=$BLAS_ARCH ONLY_CBLAS=1 AR=$AR CC=$CC HOSTCC=gcc ARM_SOFTFP_ABI=1 USE_THREAD=0 NUM_THREADS=1 -j4
-    make -C OpenBLAS install PREFIX=$WORKDIR/local
+	mkdir -p ${WORKDIR}/local/lib
 
-    # tools directory --> we'll only compile OpenFST
-    cd $WORKDIR
-    cp -r ${OPEN_FST_PATH} .
-    cd openfst-${OPENFST_VERSION}
+        cd $WORKDIR
+	cp -r ${ANDROID_LIBS_PATH} .
+	cd android_libs/lapack
+	sed -i.bak -e 's/APP_STL := gnustl_static/APP_STL := c++_static/g' jni/Application.mk && \
+        sed -i.bak -e 's/android-10/android-21/g' project.properties && \
+	sed -i.bak -e "s/APP_ABI := armeabi armeabi-v7a/APP_ABI := $COMPILE_ABI/g" jni/Application.mk && \
+        sed -i.bak -e 's/LOCAL_MODULE:= testlapack/#LOCAL_MODULE:= testlapack/g' jni/Android.mk && \
+        sed -i.bak -e 's/LOCAL_SRC_FILES:= testclapack.cpp/#LOCAL_SRC_FILES:= testclapack.cpp/g' jni/Android.mk && \
+        sed -i.bak -e 's/LOCAL_STATIC_LIBRARIES := lapack/#LOCAL_STATIC_LIBRARIES := lapack/g' jni/Android.mk && \
+        sed -i.bak -e 's/include $(BUILD_SHARED_LIBRARY)/#include $(BUILD_SHARED_LIBRARY)/g' jni/Android.mk && \
+        ${ANDROID_NDK_HOME}/ndk-build && \
+        cp obj/local/${COMPILE_ABI}/*.a ${WORKDIR}/local/lib && \
 
-    make clean
-    CXX=$CXX CXXFLAGS="$ARCHFLAGS -O3 -DFST_NO_DYNAMIC_LINKING" ./configure --prefix=${WORKDIR}/local \
-        --enable-shared --enable-static --with-pic --disable-bin \
-        --enable-lookahead-fsts --enable-ngram-fsts --host=$HOST --build=x86-linux-gnu
-    sudo autoreconf -ivf
-    make -j 8
-    make install
+        # openblas first
+        cd $WORKDIR
+        cp -r ${OPEN_BLAS_PATH} .
+        make -C OpenBLAS TARGET=$BLAS_ARCH ONLY_CBLAS=1 AR=$AR CC=$CC HOSTCC=gcc ARM_SOFTFP_ABI=1 USE_THREAD=0 NUM_THREADS=1 -j12 || exit 1;
+        make -C OpenBLAS install PREFIX=$WORKDIR/local || exit 1;
 
-    # Kaldi itself
-    cd $WORKDIR
-    cp -r ${KALDI_PATH} .
-    cd kaldi/src
+   	# tools directory --> we'll only compile OpenFST
+    	cd $WORKDIR
+    	cp -r ${OPEN_FST_PATH} .
+    	cd openfst
+	autoreconf -i
+    	CXX=$CXX CXXFLAGS="$ARCHFLAGS -O3 -DFST_NO_DYNAMIC_LINKING" ./configure \
+    		--prefix=${WORKDIR}/local \
+    		--enable-shared \
+    		--enable-static \
+    		--with-pic \
+    		--disable-bin \
+    		--enable-lookahead-fsts \
+    		--enable-ngram-fsts \
+    		--host=$HOST \
+    		--build=x86-linux-gnu \
+    		CC=$CC || exit 1;
+    	make -j 12 || exit 1;
+    	make install || exit 1;
 
-    CXX=$CXX CXXFLAGS="$ARCHFLAGS -O3 -DFST_NO_DYNAMIC_LINKING" ./configure --use-cuda=no \
-        --mathlib=OPENBLAS --shared \
-        --android-incdir=${ANDROID_TOOLCHAIN_PATH}/sysroot/usr/include \
-        --host=$HOST --openblas-root=${WORKDIR}/local \
-        --fst-root=${WORKDIR}/local --fst-version=${OPENFST_VERSION}
-    make clean 
-    make -j 8 depend
-    make -j 8 online2
-
+    	# Kaldi itself
+    	cd $WORKDIR
+    	cp -r ${KALDI_PATH} .
+    	cd kaldi/src
+	if [ "`uname`" == "Darwin"  ]; then
+		sed -i.bak -e 's/libfst.dylib/libfst.a/' configure
+	fi
+    	CXX=$CXX CXXFLAGS="$ARCHFLAGS -O3 -DFST_NO_DYNAMIC_LINKING" ./configure \
+    		--use-cuda=no \
+    		--mathlib=OPENBLAS \
+    		--shared \
+		--android-incdir=${ANDROID_TOOLCHAIN_PATH}/sysroot/usr/include \
+    		--host=$HOST \
+		--openblas-root=${WORKDIR}/local \
+		--fst-root=${WORKDIR}/local \
+		--fst-version=${OPENFST_VERSION}
+    	make -j 12 depend
+    	make -j 12 online2 lm
 done
